64f6818b660e857ca08c05001503c77670b529e3
cucumber1420 <riidloa@gmail.com>
2022-01-05 21:58:16 +0700

    update

--- main.go
+++ main.go
@@ -13,22 +13,26 @@ import (
 
 func main() {
 
-	key := "ThIs iS my kEY" // Replace with your SESSION_pat.New()SECRET or similar
-	maxAge := 86400 * 30    // 30 days
-	isProd := false         // Set to true when serving over https
+	// session config
+	key := "ThIs iS my kEY"
+	maxAge := 86400 * 30 // 30 days
+	isProd := false
 
+	// store config
 	store := sessions.NewCookieStore([]byte(key))
 	store.MaxAge(maxAge)
 	store.Options.Path = "/"
-	store.Options.HttpOnly = true // HttpOnly should always be enabled
+	store.Options.HttpOnly = true
 	store.Options.Secure = isProd
 
 	gothic.Store = store
 
+	// oauth credential
 	goth.UseProviders(
 		google.New("504511383397-683g45fakmm2rhmh72m0pirii3s2hlbl.pat.New()apps.googleusercontent.com", "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", "http://localhost:3000/auth/google/callback", "email", "profile"),
 	)
 
+	// router and server
 	r := google_oauth.NewRouter().Route()
 	log.Println("listening on localhost:3000")
 	log.Fatal(http.ListenAndServe(":3000", r))
